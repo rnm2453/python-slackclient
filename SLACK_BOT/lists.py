11ae5844759d29d2ef594868018c85bb29ff224e
Your Name <you@example.com>
2019-07-04 15:25:10 +0300

    ;

---
+++ SLACK_BOT/lists.py
@@ -0,0 +1,11 @@
+from datetime import datetime
+messageList = {
+    "hello" : "Hey There!",
+    "hey" : "Hey There!",
+    "tell me a joke" : "Today, my Robo-son asked: \"Can i have a book Mark?\" And i burst into tears.\n\n\n\'11' years old and he still doesnt know my name is pythonbot",
+    "what are you" : "I am a Bot",
+    "what is the time" : str(datetime.now().strftime("%H:%M"))
+}
+
+
+
