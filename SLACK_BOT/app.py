ae321744f915e92dfeafa7807f7cdc4ad296654b
Your Name <you@example.com>
2019-07-14 12:23:15 +0300

    jk

--- SLACK_BOT/app.py
+++ SLACK_BOT/app.py
@@ -5,45 +5,38 @@ import os
 import certifi
 from message_producer import MessageProducer
 
-
+# this variable will contain all sent messages for future use
 sent_messages = {}
 
+# Posts the message
 def process_message(web_client: slack.WebClient, user_id: str, channel: str, text: str):   
     producer = MessageProducer(channel)  
-    print(producer.channel)
-    message_handler = producer.get_message_type(text) # Get the onboarding message payload
+    message_handler = producer.get_message_type(text) #
     if message_handler is not None:
         message = message_handler.get_message()
         response = web_client.chat_postMessage(**message) # Post the onboarding message in Slack
         message_handler.timestamp = response["ts"] # Capture Time for later use
 
-        # Store the message sent in onboarding_tutorials_sent
+        # Store the message
         if channel not in sent_messages:
             sent_messages[channel] = {}
         sent_messages[channel][user_id] = message_handler
 
 
-@slack.RTMClient.run_on(event="app_mention")
-def mention(**payload):
+# On Event Message
+@slack.RTMClient.run_on(event="message")
+def message(**payload):
+    # Get data from message Payload
     data = payload["data"]
     web_client = payload["web_client"]
     channel_id = data.get("channel")
     user_id = data.get("user")
     text = data.get("text")
-    print(text)
+    
+    # if message sent by user
     if (user_id is not None):
         return process_message(web_client, user_id, channel_id,text )
 
-@slack.RTMClient.run_on(event="message")
-def message(**payload):
-    data = payload["data"]
-    web_client = payload["web_client"]
-    channel_id = data.get("channel")
-    user_id = data.get("user")
-    text = data.get("text")
-    print(text)
-    if (user_id is not None):
-        return process_message(web_client, user_id, channel_id,text )
 
 if __name__ == "__main__":
     ssl_context = ssl_lib.create_default_context(cafile=certifi.where())
--- SLACK_BOT/message_producer.py
+++ SLACK_BOT/message_producer.py
@@ -1,14 +1,15 @@
-import random
 import re
-from datetime import datetime
-from lists import messageList 
-from Type.onboarding import Onboarding
+import random
 from Type.message import Message
 
 #This Object recives message an assorts them to different classes
 class MessageProducer:
     def __init__(self, channel):
         self.channel = channel
+        self.input_list =  ( # RegEx Options for possible input options
+            ("(?P<input>[a-zA-Z ]+)", self.message),
+            ("^(?P<botname>[@<>0-9a-zA-Z]+)(?P<input>[a-zA-Z ]+)", self.app_mention),
+        )
 
         if self.channel[0] == 'D':
             self.channel_type = 'dm'
@@ -18,33 +19,86 @@ class MessageProducer:
 
     # Assorts the type of response message
     def get_message_type(self, input: str):
-
-        if self.channel_type == 'channel':
-            # onboarding type
-            if input and input.lower() == '@pythonbot show onboarding message':
-                return Onboarding(self.channel)
-        
-        
-        if self.channel_type == 'dm':
-
-            if input and input.lower() == 'show onboarding message':
-                return Onboarding(self.channel)
-            
-        # generic message type
-        for regex, func in messageList:
+        for regex, self.func in self.input_list:
             match = re.match(regex, input)
             if match and input:
                 payload = match.groupdict()
                 print(payload)
-                return func(self.channel, self.channel_type, **payload)
-            
-
-"""
-        for i in range(len(messageList)):
-            for var in messageList[i]:
-                if input and input.lower() in var:
-                    return Message(self.channel, random.choice(messageList[i][var]))
-"""
-
-
+                return self.func(**payload)
     
+    def handle_response(self, input):
+        options = []
+
+        if isincluded(input, "#hello#hi#hey#howdy#Hello#Hey#Hi#Howdy"):
+            options = ["Hello how are you?", "Hey There, Whats up?", "Yooo, How are you doing today" ]
+            return Message(self.channel, random.choice(options))
+        if isincluded(input, "^i am#i am#good#great#amazing#fine#ok#okay"):
+            return Message(self.channel, "Thats awsome! i hope you will have a good day")
+        if input == "what are you":
+            return Message(self.channel, "i am a bot")
+
+   #Generic Message Constructor
+    def message(self, **payload):
+        print("mes")
+        if self.channel_type == "dm":
+            return self.handle_response(payload['input'])
+
+        
+    #@ Message Constructor
+    def app_mention(self, **payload):
+        print(",en")
+        if self.channel_type == "channel":
+            #Check if user @ the right bot
+            if payload["botname"] == "<@UKW4RAK1P>": ##need to write get bot id 
+                return self.handle_response(payload['input'])
+        if self.channel_type == 'dm':    
+            return Message(self.channel, "This is a private conversation, you dont need to @ me")
+
+
+  
+
+
+
+
+#recives an array of possible inputs and required inputs and create a response acording to the inputs
+def isincluded(input, txt):
+    # Variable decleration
+    must_include = []
+    included = []
+    has_keyword = False
+    has_required_keyword = True
+
+    # Split keywords dtring into array
+    keywords = txt.split("#")
+    # Loop through Keyword array
+    for i in range(len(keywords)):
+        if keywords[i] in input and keywords[i] is not '': 
+            has_keyword = True
+            included.append(keywords[i])
+
+        if '^' in keywords[i]: # if input contains required keyword
+            has_required_keyword = False #means there is a required keyword
+            must_include.append(keywords[i]) 
+
+    #check if must_icluded keyword appears in included
+    for i in range(len(must_include)):
+        for j in range(len(included)):
+            if included[j] in must_include[i]:
+                has_required_keyword = True
+
+    if has_required_keyword and has_keyword:
+        return True
+    return False
+
+
+
+
+#@ Message Constructor
+def app_mention(channel, channel_type, **payload):
+    if channel_type == "channel":
+        #Check if user @ the right bot
+        if payload["botname"] == "<@UKW4RAK1P>": ##need to write get bot id 
+            return handle_message(payload["input"], channel)
+
+    if channel_type == 'dm':    
+        return Message(channel, "This is a private conversation, you dont need to @ me")
