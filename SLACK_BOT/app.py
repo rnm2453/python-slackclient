9ab7c735ef463cd6c576fde1bdee596bcb085293
Your Name <you@example.com>
2019-07-21 14:17:06 +0300

    created 'attachments' function

--- SLACK_BOT/app.py
+++ SLACK_BOT/app.py
@@ -11,20 +11,21 @@ from message_producer import MessageProducer
 sent_messages = {}
 
 # Posts the message
-def process_message(web_client: slack.WebClient, user_id: str, channel_id: str, text: str, thread_data: dict):
-    producer = MessageProducer(channel_id, thread_data)  
+def process_message(web_client: slack.WebClient, user_id: str, channel_id: str, text: str, attachments: dict):
+    producer = MessageProducer(channel_id, attachments)  
     message_handler = producer.get_message_type(text) #
     if message_handler is not None:
-        message = message_handler.get_message(thread_data)
-        response = web_client.chat_postMessage(**message) # Post the onboarding message in Slack
-        message_handler.timestamp = response["ts"] # Capture Time for later use
-
-        # Store the message
-        if channel_id not in sent_messages:
-            sent_messages[channel_id] = {}
-            sent_messages[channel_id][user_id] = message_handler
-    
-    print(sent_messages)
+        if isinstance(message_handler, list): #if array of Messages
+            for i in range(len(message_handler)): 
+                message = message_handler[i].get_message(attachments)
+                response = web_client.chat_postMessage(**message) # Post the onboarding message in Slack
+                message_handler[i].timestamp = response["ts"] # Capture Time for later use
+        else: 
+            message = message_handler.get_message(attachments)
+            response = web_client.chat_postMessage(**message) # Post the onboarding message in Slack
+            message_handler.timestamp = response["ts"] # Capture Time for later use
+        
+      
 
 
 @slack.RTMClient.run_on(event="reaction_added")
@@ -35,23 +36,10 @@ def reaction_added(**payload):
     user_id = data.get("user")
     channel_id = data.get("item").get("channel")
     text = ":" + data.get("reaction") + ":"
-    print(channel_id)
-    print(user_id)
-    print(text)
-    # Allowes to understand if thread or regular message
-    thread_data = {
-        "ts" : data.get("ts", ""),
-        "thread_ts" : data.get("thread_ts", ""),
-        "reply_users_count" : int(data.get("reply_users_count", 0)),
-        "in_thread" : False
-    }
-
-    if thread_data["thread_ts"] != "":
-        thread_data['in_thread'] = True
-
+    attachments = create_attachments(data)
     # if message sent by user
     if (user_id is not None):
-        return process_message(web_client, user_id, channel_id, text, thread_data)
+        return process_message(web_client, user_id, channel_id, text, attachments)
    
 # On Event Message
 @slack.RTMClient.run_on(event="message")
@@ -62,29 +50,32 @@ def message(**payload):
     user_id = data.get("user")
     channel_id = data.get("channel")
     text = data.get("text")
-    # Allowes to understand if thread or regular message
-    thread_data = {
-        "ts" : data.get("ts", ""),
-        "thread_ts" : data.get("thread_ts", ""),
-        "reply_users_count" : int(data.get("reply_users_count", 0)),
-        "in_thread" : False
-    }
-
-    if thread_data["thread_ts"] != "":
-        thread_data['in_thread'] = True
-    
-
+    attachments = create_attachments(data)
+   
     # if message sent by user
     if (user_id is not None):
-        return process_message(web_client, user_id, channel_id, text, thread_data)
+        return process_message(web_client, user_id, channel_id, text, attachments)
     
 
+def create_attachments(data) :
+    attachments = {
+        #thread_attachments
+        "ts": data.get("ts", ""),
+        "thread_ts" : data.get("thread_ts", ""),
+        "in_thread" : False,
+        #reaction_attachments
+        "item_user" : data.get("item_user", None)
+    }
+    if attachments["thread_ts"] != "":
+        attachments['in_thread'] = True
+    return attachments
 
 if __name__ == "__main__":
     ssl_context = ssl_lib.create_default_context(cafile=certifi.where())
     slack_token = os.environ["SLACK_BOT_TOKEN"]
     rtm_client = slack.RTMClient(token=slack_token, ssl=ssl_context)
     rtm_client.start()
+    
 
 
 
