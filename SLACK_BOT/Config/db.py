fb011d395371daa15ebafb5d9725aa49185e0dd8
Your Name <you@example.com>
2019-07-23 15:05:15 +0300

    erased file

--- SLACK_BOT/Config/db.py
+++
@@ -1,5 +0,0 @@
-import mysql.connector
-from datetime import datetime
-
-release_time = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
-conn = mysql.connector.connect(host= 'localhost', user= 'root', passwd= 'Game1234monkey', db= 'slackusers')
