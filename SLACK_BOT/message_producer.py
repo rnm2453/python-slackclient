24e8d7ebe1bdc003d7c1c4c8f8e655714d9548c9
Your Name <you@example.com>
2019-07-28 11:40:21 +0300

    added release from database

--- SLACK_BOT/message_producer.py
+++ SLACK_BOT/message_producer.py
@@ -2,34 +2,42 @@ import re
 import os
 import random
 from bot import Bot
+from database import Database
 from Type.message import Message
 from datetime import datetime, timedelta
-from database_alternator import database_alternator
 
 # Create Connection To Data Base
-db = database_alternator('localhost', 'root', 'Game1234monkey', 'slackusers')
+db = Database('localhost', 'root', 'Game1234monkey', 'slackusers')
+
+# refresh the database
+db.refresh_occupied()
 
 # Create Connection To Bot
 slack_token = os.environ["SLACK_BOT_TOKEN"]
 bot = Bot(slack_token)
-bot_username = bot.get_username
+
+
 
 #This Object recives message and assorts them to different classes
 class MessageProducer:
     def __init__(self, channel: str, user_id: str, attachments : dict):
+
         self.user_id = user_id
         self.username = bot.get_username(user_id)
+        self.bot_username = "<@" +  str(bot.get_ID()) + ">"
+
         self.channel = channel
         self.channel_type = self.get_channel_type()
-        self.bot_username = "<@" +  str(bot_username) + ">"
+
         self.thread_ts = attachments["thread_ts"]
         self.in_thread = attachments["in_thread"]
         self.item_user = attachments["item_user"]
         
         self.input_list =  ( # RegEx  for possible input options
             (r"((?P<botname>[@<>0-9A-Z]+)\s+)?(?P<input>(?:create|insert|add)\s(?P<item>[a-z]+)(?:.+)(?:database))", self.insert),
+            (r"((?P<botname>[@<>0-9A-Z]+)\s+)?(?P<input>(?:release)\s(?P<item>[a-z]+)(?:.+)(?:database))", self.release),    
             (r"((?P<botname>[@<>0-9A-Z]+)\s+)?(?P<input>(?:take|give me)\s(?P<item>[a-z]+)(?:.+)(?:database\s)[a-zA-z\s]*(?P<time>(?:[0-9]*))(?:\s*)(?P<type>(?:(min|minutes|hour|hours|day|days))))", self.take),    
-            (r"((?P<botname>[@<>0-9A-Z]+)\s+)?(?P<input>[:!^_a-zA-Z\s@]+)", self.message),
+            (r"((?P<botname>[@<>0-9A-Z]+)\s+)?(?P<input>[!^_a-zA-Z\s@]+)", self.message),
             (r"((?P<botname>[@<>0-9a-zA-Z]+)\s+)?(?P<input>[:^_a-z ]+)", self.reaction_added) 
         )
     
@@ -94,6 +102,7 @@ class MessageProducer:
                 "add to database: 'insert/create/add <item_name> ... database\n" +
                 "take an item: 'take\\/give me <item_name>\n ... database for [number] minutes/hours/days" +
                 "remove an item 'remove/erase/delete <item_name> from database\n")
+        return Message(self.channel, "I can't understand you. type '!help'")
        ### TO RETURN A LIST OF MESSAGE USE THE FOLLOWING SYNTAX
        #return [Message(self.channel, ":hushed:"), Message(self.channel, "I Like This Emoji")]
 
@@ -102,12 +111,19 @@ class MessageProducer:
         db.insert(payload['item'], "", "", "")
         return Message(self.channel, "Record inserted successfully into database")
 
+    def release(self, **payload):
+        product = db.get_data_by_name(payload['item'])
+        if product['occupied_by'] == self.username:
+            db.update(payload['item'], "", "", "")
+        print("record released")
+
     def take(self, **payload):
         print("reached")
-        if db.is_occupied(payload['item']):
+        if db.is_occupied_by_name(payload['item']):
+            print("passed")
             return [
                 Message(self.channel, "This Item is currently used"),
-                Message(self.channel, "it will be release in " + "insert time")
+                Message(self.channel, "it will be released in " + str(db.delta_release_by_name(payload['item'])))
             ]
         else:
             input = payload['type']
@@ -115,14 +131,15 @@ class MessageProducer:
             if isincluded(input, "#min#minutes"):
                 release_time = current_time + timedelta(minutes= int(payload['time']))
             elif isincluded(input, "#hour#hours"):
-                release_time = current_time + timedelta(hours= payload['time'])
+                release_time = current_time + timedelta(hours= int(payload['time']))
             elif isincluded(input, "#day#days"):
-                release_time = current_time + timedelta(days= payload['time'])
+                release_time = current_time + timedelta(days= int(payload['time']))
             else:
                 return Message(self.channel, "I dont know what" + payload['type] + "means'])
 
             db.update(payload['item'], self.username, current_time, release_time)
             return Message(self.channel, f"You Successfully Took the {payload['item']} for {payload['time']} {payload['type']}")
+   
    #Generic Message Constructor
     def message(self, **payload):
         if self.channel_type == "dm":
@@ -136,14 +153,13 @@ class MessageProducer:
      
 
     def reaction_added(self, **payload):
+        print("passed")
         if self.channel_type == "dm":
             return Message(self.channel, ":smile:")
 
         elif self.channel_type == "channel":       
             if self.item_user == None: #This means that the reaction was on a Bot Message 
                 return [Message(self.channel, ":hushed:"), Message(self.channel, "I Like This Emoji")]
-
-
 
 
 
